ff34eb6ef0467d53d7522c96894009b245b71ec8
JuanGaz2403 <juan.garces.penafiel@udla.edu.ec>
2025-06-10 16:02:19 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,45 @@
-#include
+#include <stdio.h>
+#include "funciones.h"
+
+int main() {
+    Libro biblioteca[MAX_LIBROS];
+    int contador = 0; // Contador de libros registrados
+    int opcion;
+
+    do {
+        printf("\n--- Biblioteca ---\n");
+        printf("1. Registrar libro\n");
+        printf("2. Mostrar lista de libros\n");
+        printf("3. Buscar libro\n");
+        printf("4. Actualizar estado de un libro\n");
+        printf("5. Eliminar libro\n");
+        printf("6. Salir\n");
+        printf("Seleccione una opcion: ");
+        scanf("%d", &opcion);
+
+        switch (opcion) {
+            case 1:
+                registrarLibro(biblioteca, &contador);
+                break;
+            case 2:
+                mostrarLibros(biblioteca, contador);
+                break;
+            case 3:
+                buscarLibro(biblioteca, contador);
+                break;
+            case 4:
+                actualizarEstado(biblioteca, contador);
+                break;
+            case 5:
+                eliminarLibro(biblioteca, &contador);
+                break;
+            case 6:
+                printf("Saliendo del programa...\n");
+                break;
+            default:
+                printf("Opción invalida. Intente nuevamente.\n");
+        }
+    } while (opcion != 6);
+
+    return 0;
+}
